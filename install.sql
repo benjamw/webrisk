2b61f79a814ffe9e843a9c488d463f4391a46fb6
benjam <benjamwelker@gmail.com>
2015-01-24 10:52:37 -0700

    update schema

--- install.sql
+++ install.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 15, 2015 at 10:25 PM
+-- Generation Time: Jan 24, 2015 at 10:38 AM
 -- Server version: 5.6.20-log
 -- PHP Version: 5.6.3
 
@@ -24,144 +24,145 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `player` (
 	`player_id` int(11) unsigned NOT NULL,
-	`username` varchar(20) COLLATE latin1_general_ci NOT NULL DEFAULT '',
-	`first_name` varchar(20) COLLATE latin1_general_ci DEFAULT NULL,
-	`last_name` varchar(20) COLLATE latin1_general_ci DEFAULT NULL,
-	`email` varchar(100) COLLATE latin1_general_ci NOT NULL DEFAULT '',
-	`timezone` varchar(255) COLLATE latin1_general_ci NOT NULL DEFAULT 'UTC',
+	`username` varchar(20) COLLATE utf8_general_ci NOT NULL DEFAULT '',
+	`first_name` varchar(20) COLLATE utf8_general_ci DEFAULT NULL,
+	`last_name` varchar(20) COLLATE utf8_general_ci DEFAULT NULL,
+	`email` varchar(100) COLLATE utf8_general_ci NOT NULL DEFAULT '',
+	`timezone` varchar(255) COLLATE utf8_general_ci NOT NULL DEFAULT 'UTC',
 	`is_admin` tinyint(1) unsigned NOT NULL DEFAULT '0',
-	`password` varchar(32) COLLATE latin1_general_ci NOT NULL DEFAULT '',
-	`alt_pass` varchar(32) COLLATE latin1_general_ci NOT NULL DEFAULT '',
-	`ident` varchar(32) COLLATE latin1_general_ci DEFAULT NULL,
-	`token` varchar(32) COLLATE latin1_general_ci DEFAULT NULL,
+	`password` varchar(32) COLLATE utf8_general_ci NOT NULL DEFAULT '',
+	`alt_pass` varchar(32) COLLATE utf8_general_ci NOT NULL DEFAULT '',
+	`ident` varchar(32) COLLATE utf8_general_ci DEFAULT NULL,
+	`token` varchar(32) COLLATE utf8_general_ci DEFAULT NULL,
 	`create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`is_approved` tinyint(1) unsigned NOT NULL DEFAULT '0'
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wr_chat`
+-- Table structure for table `wr2_chat`
 --
 
-DROP TABLE IF EXISTS `wr_chat`;
-CREATE TABLE IF NOT EXISTS `wr_chat` (
+DROP TABLE IF EXISTS `wr2_chat`;
+CREATE TABLE IF NOT EXISTS `wr2_chat` (
 	`chat_id` int(10) unsigned NOT NULL,
-	`message` text COLLATE latin1_general_ci NOT NULL,
+	`message` text COLLATE utf8_general_ci NOT NULL,
 	`from_id` int(10) unsigned NOT NULL DEFAULT '0',
 	`game_id` int(10) unsigned NOT NULL DEFAULT '0',
 	`private` tinyint(1) unsigned NOT NULL DEFAULT '0',
 	`create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wr_game`
+-- Table structure for table `wr2_game`
 --
 
-DROP TABLE IF EXISTS `wr_game`;
-CREATE TABLE IF NOT EXISTS `wr_game` (
+DROP TABLE IF EXISTS `wr2_game`;
+CREATE TABLE IF NOT EXISTS `wr2_game` (
 	`game_id` int(11) unsigned NOT NULL,
 	`host_id` int(11) unsigned NOT NULL DEFAULT '0',
-	`name` varchar(255) COLLATE latin1_general_ci NOT NULL DEFAULT '',
-	`password` varchar(32) COLLATE latin1_general_ci DEFAULT NULL,
+	`name` varchar(255) COLLATE utf8_general_ci NOT NULL DEFAULT '',
+	`password` varchar(32) COLLATE utf8_general_ci DEFAULT NULL,
 	`capacity` tinyint(1) unsigned NOT NULL DEFAULT '2',
 	`time_limit` tinyint(2) unsigned DEFAULT NULL,
 	`allow_kibitz` tinyint(1) NOT NULL DEFAULT '0',
-	`game_type` varchar(255) COLLATE latin1_general_ci DEFAULT 'Original',
+	`game_type` varchar(255) COLLATE utf8_general_ci DEFAULT 'Original',
 	`next_bonus` tinyint(3) unsigned NOT NULL DEFAULT '4',
-	`state` enum('Waiting','Placing','Playing','Finished') COLLATE latin1_general_ci NOT NULL DEFAULT 'Waiting',
-	`extra_info` text COLLATE latin1_general_ci,
+	`state` enum('Waiting','Placing','Playing','Finished') COLLATE utf8_general_ci NOT NULL DEFAULT 'Waiting',
+	`extra_info` text COLLATE utf8_general_ci,
+	`game_settings` text COLLATE utf8_general_ci NOT NULL,
 	`paused` tinyint(1) NOT NULL DEFAULT '0',
 	`create_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
 	`modify_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wr_game_land`
+-- Table structure for table `wr2_game_land`
 --
 
-DROP TABLE IF EXISTS `wr_game_land`;
-CREATE TABLE IF NOT EXISTS `wr_game_land` (
+DROP TABLE IF EXISTS `wr2_game_land`;
+CREATE TABLE IF NOT EXISTS `wr2_game_land` (
 	`game_id` int(10) unsigned NOT NULL DEFAULT '0',
 	`land_id` int(10) unsigned NOT NULL DEFAULT '0',
 	`player_id` int(10) unsigned NOT NULL DEFAULT '0',
 	`armies` smallint(5) unsigned NOT NULL DEFAULT '0'
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wr_game_log`
+-- Table structure for table `wr2_game_log`
 --
 
-DROP TABLE IF EXISTS `wr_game_log`;
-CREATE TABLE IF NOT EXISTS `wr_game_log` (
+DROP TABLE IF EXISTS `wr2_game_log`;
+CREATE TABLE IF NOT EXISTS `wr2_game_log` (
 	`game_id` int(11) unsigned NOT NULL DEFAULT '0',
-	`data` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
+	`data` varchar(255) COLLATE utf8_general_ci DEFAULT NULL,
 	`create_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
 	`microsecond` int(10) unsigned NOT NULL DEFAULT '0'
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wr_game_nudge`
+-- Table structure for table `wr2_game_nudge`
 --
 
-DROP TABLE IF EXISTS `wr_game_nudge`;
-CREATE TABLE IF NOT EXISTS `wr_game_nudge` (
+DROP TABLE IF EXISTS `wr2_game_nudge`;
+CREATE TABLE IF NOT EXISTS `wr2_game_nudge` (
 	`game_id` int(10) unsigned NOT NULL DEFAULT '0',
 	`player_id` int(10) unsigned NOT NULL DEFAULT '0',
 	`nudged` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wr_game_player`
+-- Table structure for table `wr2_game_player`
 --
 
-DROP TABLE IF EXISTS `wr_game_player`;
-CREATE TABLE IF NOT EXISTS `wr_game_player` (
+DROP TABLE IF EXISTS `wr2_game_player`;
+CREATE TABLE IF NOT EXISTS `wr2_game_player` (
 	`game_id` int(11) unsigned NOT NULL DEFAULT '0',
 	`player_id` int(11) unsigned NOT NULL DEFAULT '0',
 	`order_num` tinyint(1) unsigned NOT NULL DEFAULT '0',
-	`color` varchar(10) COLLATE latin1_general_ci NOT NULL DEFAULT '',
-	`cards` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
+	`color` varchar(10) COLLATE utf8_general_ci NOT NULL DEFAULT '',
+	`cards` varchar(255) COLLATE utf8_general_ci DEFAULT NULL,
 	`armies` smallint(5) unsigned NOT NULL DEFAULT '0',
-	`state` enum('Waiting','Trading','Placing','Attacking','Occupying','Fortifying','Resigned','Dead') COLLATE latin1_general_ci NOT NULL DEFAULT 'Waiting',
+	`state` enum('Waiting','Trading','Placing','Attacking','Occupying','Fortifying','Resigned','Dead') COLLATE utf8_general_ci NOT NULL DEFAULT 'Waiting',
 	`get_card` tinyint(1) unsigned NOT NULL DEFAULT '0',
 	`forced` tinyint(1) unsigned NOT NULL DEFAULT '0',
-	`extra_info` text COLLATE latin1_general_ci,
+	`extra_info` text COLLATE utf8_general_ci,
 	`move_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wr_message`
+-- Table structure for table `wr2_message`
 --
 
-DROP TABLE IF EXISTS `wr_message`;
-CREATE TABLE IF NOT EXISTS `wr_message` (
+DROP TABLE IF EXISTS `wr2_message`;
+CREATE TABLE IF NOT EXISTS `wr2_message` (
 	`message_id` int(10) unsigned NOT NULL,
-	`subject` varchar(255) COLLATE latin1_general_ci NOT NULL DEFAULT '',
-	`message` text COLLATE latin1_general_ci NOT NULL,
+	`subject` varchar(255) COLLATE utf8_general_ci NOT NULL DEFAULT '',
+	`message` text COLLATE utf8_general_ci NOT NULL,
 	`create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wr_message_glue`
+-- Table structure for table `wr2_message_glue`
 --
 
-DROP TABLE IF EXISTS `wr_message_glue`;
-CREATE TABLE IF NOT EXISTS `wr_message_glue` (
+DROP TABLE IF EXISTS `wr2_message_glue`;
+CREATE TABLE IF NOT EXISTS `wr2_message_glue` (
 	`message_glue_id` int(10) unsigned NOT NULL,
 	`message_id` int(10) unsigned NOT NULL DEFAULT '0',
 	`from_id` int(10) unsigned NOT NULL DEFAULT '0',
@@ -171,16 +172,16 @@ CREATE TABLE IF NOT EXISTS `wr_message_glue` (
 	`view_date` datetime DEFAULT NULL,
 	`create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`deleted` tinyint(1) unsigned NOT NULL DEFAULT '0'
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wr_roll_log`
+-- Table structure for table `wr2_roll_log`
 --
 
-DROP TABLE IF EXISTS `wr_roll_log`;
-CREATE TABLE IF NOT EXISTS `wr_roll_log` (
+DROP TABLE IF EXISTS `wr2_roll_log`;
+CREATE TABLE IF NOT EXISTS `wr2_roll_log` (
 	`id` int(11) unsigned NOT NULL,
 	`attack_1` tinyint(1) unsigned NOT NULL DEFAULT '0',
 	`attack_2` tinyint(1) unsigned DEFAULT NULL,
@@ -192,22 +193,22 @@ CREATE TABLE IF NOT EXISTS `wr_roll_log` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wr_settings`
+-- Table structure for table `wr2_settings`
 --
 
-DROP TABLE IF EXISTS `wr_settings`;
-CREATE TABLE IF NOT EXISTS `wr_settings` (
-	`setting` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL DEFAULT '',
-	`value` text CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-	`notes` text CHARACTER SET latin1 COLLATE latin1_general_ci,
+DROP TABLE IF EXISTS `wr2_settings`;
+CREATE TABLE IF NOT EXISTS `wr2_settings` (
+	`setting` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+	`value` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+	`notes` text CHARACTER SET utf8 COLLATE utf8_general_ci,
 	`sort` smallint(5) unsigned NOT NULL DEFAULT '0'
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `wr_settings`
+-- Dumping data for table `wr2_settings`
 --
 
-INSERT INTO `wr_settings` (`setting`, `value`, `notes`, `sort`) VALUES
+INSERT INTO `wr2_settings` (`setting`, `value`, `notes`, `sort`) VALUES
 	('site_name', 'Your Site Name', 'The name of your site', 10),
 	('default_color', 'c_yellow_black.css', 'The default theme color for the game pages', 20),
 	('nav_links', '<!-- your links here -->', 'HTML code for your site''s navigation links to display on the game pages', 30),
@@ -233,22 +234,23 @@ INSERT INTO `wr_settings` (`setting`, `value`, `notes`, `sort`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wr_wr_player`
+-- Table structure for table `wr2_wr2_player`
 --
 
-DROP TABLE IF EXISTS `wr_wr_player`;
-CREATE TABLE IF NOT EXISTS `wr_wr_player` (
+DROP TABLE IF EXISTS `wr2_wr2_player`;
+CREATE TABLE IF NOT EXISTS `wr2_wr2_player` (
 	`player_id` int(11) unsigned NOT NULL DEFAULT '0',
+	`game_settings` text COLLATE utf8_general_ci NOT NULL,
 	`is_admin` tinyint(1) unsigned NOT NULL DEFAULT '0',
 	`allow_email` tinyint(1) unsigned NOT NULL DEFAULT '1',
-	`color` varchar(25) COLLATE latin1_general_ci NOT NULL DEFAULT 'yellow_black',
+	`color` varchar(25) COLLATE utf8_general_ci NOT NULL DEFAULT 'yellow_black',
 	`invite_opt_out` tinyint(1) NOT NULL DEFAULT '0',
 	`max_games` tinyint(3) unsigned NOT NULL DEFAULT '0',
 	`wins` smallint(5) unsigned NOT NULL DEFAULT '0',
 	`kills` smallint(5) unsigned NOT NULL DEFAULT '0',
 	`losses` smallint(5) unsigned NOT NULL DEFAULT '0',
 	`last_online` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -263,54 +265,54 @@ ALTER TABLE `player`
 	ADD UNIQUE KEY `email` (`email`);
 
 --
--- Indexes for table `wr_chat`
+-- Indexes for table `wr2_chat`
 --
-ALTER TABLE `wr_chat`
+ALTER TABLE `wr2_chat`
 	ADD PRIMARY KEY (`chat_id`),
 	ADD KEY `game_id` (`game_id`),
 	ADD KEY `private` (`private`),
 	ADD KEY `from_id` (`from_id`);
 
 --
--- Indexes for table `wr_game`
+-- Indexes for table `wr2_game`
 --
-ALTER TABLE `wr_game`
+ALTER TABLE `wr2_game`
 	ADD PRIMARY KEY (`game_id`);
 
 --
--- Indexes for table `wr_game_land`
+-- Indexes for table `wr2_game_land`
 --
-ALTER TABLE `wr_game_land`
+ALTER TABLE `wr2_game_land`
 	ADD UNIQUE KEY `game_land` (`game_id`,`land_id`);
 
 --
--- Indexes for table `wr_game_log`
+-- Indexes for table `wr2_game_log`
 --
-ALTER TABLE `wr_game_log`
+ALTER TABLE `wr2_game_log`
 	ADD UNIQUE KEY `game_id` (`game_id`,`create_date`,`microsecond`);
 
 --
--- Indexes for table `wr_game_nudge`
+-- Indexes for table `wr2_game_nudge`
 --
-ALTER TABLE `wr_game_nudge`
+ALTER TABLE `wr2_game_nudge`
 	ADD UNIQUE KEY `game_player` (`game_id`,`player_id`);
 
 --
--- Indexes for table `wr_game_player`
+-- Indexes for table `wr2_game_player`
 --
-ALTER TABLE `wr_game_player`
+ALTER TABLE `wr2_game_player`
 	ADD UNIQUE KEY `game_player` (`game_id`,`player_id`);
 
 --
--- Indexes for table `wr_message`
+-- Indexes for table `wr2_message`
 --
-ALTER TABLE `wr_message`
+ALTER TABLE `wr2_message`
 	ADD PRIMARY KEY (`message_id`);
 
 --
--- Indexes for table `wr_message_glue`
+-- Indexes for table `wr2_message_glue`
 --
-ALTER TABLE `wr_message_glue`
+ALTER TABLE `wr2_message_glue`
 	ADD PRIMARY KEY (`message_glue_id`),
 	ADD KEY `outbox` (`from_id`,`message_id`),
 	ADD KEY `created` (`create_date`),
@@ -319,22 +321,22 @@ ALTER TABLE `wr_message_glue`
 	ADD KEY `message_id` (`message_id`,`to_id`);
 
 --
--- Indexes for table `wr_roll_log`
+-- Indexes for table `wr2_roll_log`
 --
-ALTER TABLE `wr_roll_log`
+ALTER TABLE `wr2_roll_log`
 	ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `wr_settings`
+-- Indexes for table `wr2_settings`
 --
-ALTER TABLE `wr_settings`
+ALTER TABLE `wr2_settings`
 	ADD UNIQUE KEY `setting` (`setting`),
 	ADD KEY `sort` (`sort`);
 
 --
--- Indexes for table `wr_wr_player`
+-- Indexes for table `wr2_wr2_player`
 --
-ALTER TABLE `wr_wr_player`
+ALTER TABLE `wr2_wr2_player`
 	ADD UNIQUE KEY `id` (`player_id`);
 
 --
@@ -347,27 +349,27 @@ ALTER TABLE `wr_wr_player`
 ALTER TABLE `player`
 	MODIFY `player_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
--- AUTO_INCREMENT for table `wr_chat`
+-- AUTO_INCREMENT for table `wr2_chat`
 --
-ALTER TABLE `wr_chat`
+ALTER TABLE `wr2_chat`
 	MODIFY `chat_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
--- AUTO_INCREMENT for table `wr_game`
+-- AUTO_INCREMENT for table `wr2_game`
 --
-ALTER TABLE `wr_game`
+ALTER TABLE `wr2_game`
 	MODIFY `game_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
--- AUTO_INCREMENT for table `wr_message`
+-- AUTO_INCREMENT for table `wr2_message`
 --
-ALTER TABLE `wr_message`
+ALTER TABLE `wr2_message`
 	MODIFY `message_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
--- AUTO_INCREMENT for table `wr_message_glue`
+-- AUTO_INCREMENT for table `wr2_message_glue`
 --
-ALTER TABLE `wr_message_glue`
+ALTER TABLE `wr2_message_glue`
 	MODIFY `message_glue_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
--- AUTO_INCREMENT for table `wr_roll_log`
+-- AUTO_INCREMENT for table `wr2_roll_log`
 --
-ALTER TABLE `wr_roll_log`
+ALTER TABLE `wr2_roll_log`
 	MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
